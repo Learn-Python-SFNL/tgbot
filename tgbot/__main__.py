e113c1064e1bfd6d4a98c546b36d11da93fcb577
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-04-01 13:15:55 +0300

    fix, add linters

--- tgbot/__main__.py
+++ tgbot/__main__.py
@@ -1,8 +1,6 @@
 import logging
+
 import httpx
-
-from flask import request
-
 from telegram.ext import CommandHandler, Updater
 
 from config import config
@@ -18,14 +16,16 @@ def get_categories():
     response.raise_for_status()
     return response.json()
 
+
 def post_product(uid, product):
     products = {
-        "category_id": uid,
-        "title": product,
+        'category_id': uid,
+        'title': product,
     }
     response = httpx.post('http://127.0.0.1:8000/api/v1/products/', json=products)
     response.raise_for_status()
     return response.json()
+
 
 def user_registration(update, context):
     logger.info('Вызван /start')
@@ -35,50 +35,16 @@ def user_registration(update, context):
     tgid = user.id
     first_name = user.first_name
     context.bot.send_message(
-                chat_id=update.message.chat_id,
-                text=f'Привет {first_name}! Это проект Swap4newlife.\n'
-                f'Я создан для того, чтобы подарить вещам вторую жизнь.\n'
-                f'Если у тебя накопилось много ненужных вещей, '
-                f'я помогу тебе обменять их на нужные!'
-            )
+        chat_id=update.message.chat_id,
+        text=f'Привет {first_name}! Это проект Swap4newlife.\n'
+        f'Я создан для того, чтобы подарить вещам вторую жизнь.\n'
+        f'Если у тебя накопилось много ненужных вещей, '
+        f'я помогу тебе обменять их на нужные!'
+    )
     api.users.registrate(username=username, tgid=tgid)
 
 
 def add_product(update, context):
-    categories = [
-  {
-    "id": 4,
-    "title": "Комнатные растения"
-  },
-  {
-    "id": 5,
-    "title": "Товары для животных"
-  },
-  {
-    "id": 6,
-    "title": "Женская одежда"
-  },
-  {
-    "id": 7,
-    "title": "Мужская одежда"
-  },
-  {
-    "id": 13,
-    "title": "Ковры"
-  },
-  {
-    "id": 14,
-    "title": "Самые лучшие Ковры"
-  },
-  {
-    "id": 15,
-    "title": "Бархатные тяги"
-  },
-  {
-    "id": 9,
-    "title": "Плюшевые динозавры"
-  }
-]
     categories = get_categories()
     title = categories['title']
 
@@ -90,15 +56,16 @@ def choose_categoties(update, context):
         message = 'Такой категории нет'
     update.message.reply_text(message)
 
+
 def main():
 
     mybot = Updater(config.api_key, use_context=True)
-    dp = mybot.dispatcher
+    dp = mybot.dispatcher  # type: ignore
     dp.add_handler(CommandHandler('start', user_registration))
     logging.info('Бот Стартовал')
     mybot.start_polling()
     mybot.idle()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
