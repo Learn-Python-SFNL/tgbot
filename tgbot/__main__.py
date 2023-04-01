fe6a6db24349fd9def0adf90bf0a351a5d6e6269
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-04-01 12:54:41 +0300

    resolve conflicts

--- tgbot/__main__.py
+++ tgbot/__main__.py
@@ -1,4 +1,7 @@
 import logging
+import httpx
+
+from flask import request
 
 from telegram.ext import CommandHandler, Updater
 
@@ -9,6 +12,20 @@ logger = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
+
+def get_categories():
+    response = httpx.get('http://127.0.0.1:8000/api/v1/categories/')
+    response.raise_for_status()
+    return response.json()
+
+def post_product(uid, product):
+    products = {
+        "category_id": uid,
+        "title": product,
+    }
+    response = httpx.post('http://127.0.0.1:8000/api/v1/products/', json=products)
+    response.raise_for_status()
+    return response.json()
 
 def user_registration(update, context):
     logger.info('Вызван /start')
@@ -26,6 +43,52 @@ def user_registration(update, context):
             )
     api.users.registrate(username=username, tgid=tgid)
 
+
+def add_product(update, context):
+    categories = [
+  {
+    "id": 4,
+    "title": "Комнатные растения"
+  },
+  {
+    "id": 5,
+    "title": "Товары для животных"
+  },
+  {
+    "id": 6,
+    "title": "Женская одежда"
+  },
+  {
+    "id": 7,
+    "title": "Мужская одежда"
+  },
+  {
+    "id": 13,
+    "title": "Ковры"
+  },
+  {
+    "id": 14,
+    "title": "Самые лучшие Ковры"
+  },
+  {
+    "id": 15,
+    "title": "Бархатные тяги"
+  },
+  {
+    "id": 9,
+    "title": "Плюшевые динозавры"
+  }
+]
+    categories = get_categories()
+    title = categories['title']
+
+
+def choose_categoties(update, context):
+    if context.args:
+        message = 'Поздравляю, вы выбрали категорию!'
+    else:
+        message = 'Такой категории нет'
+    update.message.reply_text(message)
 
 def main():
 
