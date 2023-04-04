cea432fcb2f493e37d604c1ee8c3c4086ab1068b
Игорь Капинкин <i.kapinkin@mail.ru>
2023-04-04 18:53:44 +0300

    chore: поправил config

--- config.py
+++ config.py
@@ -5,22 +5,14 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     api_key: str
-
-
-def load():
-    return Config(api_key=os.environ['API_KEY'])
-
-
-config = load()
-
-
-@dataclass
-class ConfigApi:
     http_key: str
 
 
-def load_api():
-    return ConfigApi(http_key=os.environ['HTTP_KEY'])
+def load():
+    return Config(
+        api_key=os.environ['API_KEY'],
+        http_key=os.environ['HTTP_KEY'],
+    )
 
 
-config_api = load_api()
+config = load()
