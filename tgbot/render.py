088676412101af657896f7c12f044b5942fb0782
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-04-06 20:51:04 +0300

    chore: fix and add function

--- tgbot/render.py
+++ tgbot/render.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 jenv = Environment(
@@ -15,3 +17,13 @@ def great_user(user):
 def show_categories(categories):
     template2 = jenv.get_template('all_categories.j2')
     return template2.render(categories=categories)
+
+
+def show_add_product(product: dict[str, Any], category: dict[str, Any]):
+    template3 = jenv.get_template('add_product.j2')
+    return template3.render(product_name=product['title'], category_name=category['title'])
+
+
+def noname_category(category_name):
+    template4 = jenv.get_template('no_category_text.j2')
+    return template4.render(category_name=category_name)
