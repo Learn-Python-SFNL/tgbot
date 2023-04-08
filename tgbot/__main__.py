d41564344b17d109a794f462f6a566f8e135cc58
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-04-08 14:31:25 +0300

    chore: fix

--- tgbot/__main__.py
+++ tgbot/__main__.py
@@ -3,19 +3,19 @@ import logging
 from telegram.ext import CommandHandler, Updater
 
 from config import config
-from tgbot.handlers import add_product, user_registration
+from tgbot.handlers import add_product, user_registration, want_products_reply
 
 logger = logging.getLogger(__name__)
-
-logging.basicConfig(level=logging.INFO)
 
 
 def main():
 
+    logging.basicConfig(level=config.log_level)
     mybot = Updater(config.api_key, use_context=True)
     dp = mybot.dispatcher  # type: ignore
     dp.add_handler(CommandHandler('start', user_registration))
     dp.add_handler(CommandHandler('add', add_product))
+    dp.add_handler(CommandHandler('want', want_products_reply))
     logging.info('Бот Стартовал')
     mybot.start_polling()
     mybot.idle()
