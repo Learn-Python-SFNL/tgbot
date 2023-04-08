856c6d5973b211d4895ab1e8651c777c89fc90b3
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-04-08 14:32:20 +0300

    chore: want_products_reply

--- tgbot/handlers.py
+++ tgbot/handlers.py
@@ -3,11 +3,15 @@ import logging
 from tgbot.api import api
 from tgbot.errors import IncorrectAddCmdError
 from tgbot.products import parse_add_product_cmd
-from tgbot.render import great_user, noname_category, show_add_product, show_categories
+from tgbot.render import (
+    great_user,
+    noname_category,
+    show_add_product,
+    show_categories,
+    show_want_handler_reply,
+)
 
 logger = logging.getLogger(__name__)
-
-logging.basicConfig(level=logging.INFO)
 
 
 def user_registration(update, context):
@@ -43,3 +47,28 @@ def add_product(update, context):
     product = api.products.add(categories[0]['id'], product_name)
     product_add = show_add_product(product, categories[0])
     update.message.reply_text(product_add)
+
+
+def want_products_reply(update, context):
+    """Вернуть список продуктов по категории Учебник.
+
+    /want Учебник
+
+    Книга по Python
+    Книга по цветочкам
+    Зельеваренье
+    """
+    cmd: str = update.message.text
+    logger.info('Вызванна команда %s', cmd)
+    category_name = ' '.join(cmd.split(' ')[1:])
+    categories = api.categories.get_categories_by_name(category_name)
+    if not categories:
+        update.message.reply_text('Извините, такой категории нет, Вика еще учится')
+        return
+
+    # TODO: обработать случай с несколькими категориями
+    category = categories[0]
+    products = api.categories.get_products(category['id'])
+
+    message = show_want_handler_reply(categories[0], products)
+    update.message.reply_text(message)
