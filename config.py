ae17461060655acea848b6516a5d4deacc47617b
Vika Trofimova <vika_trofimova_1999@mail.ru>
2023-04-08 14:30:52 +0300

    chore: refactoring

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ from dataclasses import dataclass
 class Config:
     api_key: str
     http_key: str
+    log_level: str
 
 
 def load():
     return Config(
         api_key=os.environ['API_KEY'],
         http_key=os.environ['HTTP_KEY'],
+        log_level=os.environ['LOG_LEVEL'],
     )
 
 
